ac73af498944e1ae347790b2be4b480dacf47bf1
davide-guerra <davide.guerra@yahoo.it>
2019-01-29 16:39:54 +0100

    Autorizzazioni e associazioni per users e content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_24_101631) do
+ActiveRecord::Schema.define(version: 2019_01_29_104824) do
 
   create_table "contents", force: :cascade do |t|
     t.string "titolo"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_01_24_101631) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
